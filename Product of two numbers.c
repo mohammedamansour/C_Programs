e7ee1bb9d1a75a47c40c219453274611457bf23a
Mohammed Amansour <amansour.mohammed@etu.uae.ac.ma>
2023-07-08 17:05:29 +0100

    Add files via upload
    I uploaded the c file of a programe that calculates the product of two numbers given by the user

---
+++ Product of two numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+main()
+{
+    int N1, N2, M;
+    printf("I will multiply two numbers together \nType the first number : ");scanf("%d", &N1);
+    printf("Type the second number : ");scanf("%d", &N2);
+    M = N1 * N2;
+    printf("The result is : %d", M);
+}
